a020734578905ed55f056bd3813fe0df0503181e
Mam1004 <77121389+Mam1004@users.noreply.github.com>
2022-03-09 11:53:08 -0600

    Update folder_refresh_individual lag.py

--- folder_refresh_individual lag.py
+++ folder_refresh_individual lag.py
@@ -2,7 +2,7 @@
 """
 Created on Wed Dec 23 12:09:09 2020
 
-@author: Mam1004
+@author: s0046998
 """
 
 import win32com.client
@@ -11,32 +11,41 @@ import glob
 # import time
 
 # for WorkingFile in os.listdir("C:\\Users\\techy\\OneDrive\\Desktop\\python\\Data_files\\cust-geo"):
+File_path=r"U:\Claims\Reporting\Lags-Individual\2022"#change the year in Dec
+#File_path=r"M:\python\individual_lag"
 
-File_path=r"C:\\Users\\techy\\OneDrive\\Desktop\\python\\Data_files\\cust-geo"
 os.chdir(File_path)
-xlapp = win32com.client.DispatchEx("Excel.Application")
 names=[]
-for FileList in glob.glob('*.xlsm'):
-    if  not FileList.startswith('~$')  :
-        names.append(FileList)
-  
-xlapp.Quit()
-
-print(names)
 
 xlapp = win32com.client.DispatchEx("Excel.Application")
-for i in range(len(names)):
-        #print(i)
+
+def get_path():
+    
+     os.chdir(File_path)
+     xlapp = win32com.client.DispatchEx("Excel.Application")
+     
+     for FileList in glob.glob('*.xlsm'):
+        if  not FileList.startswith('~$')  :
+           names.append(File_path+"\\"+FileList)
+     xlapp.Quit()
+     return names
+   
+def Refresh_file(names):
+     for FileList in names:
         pw_str = 'locked'
-        wb = xlapp.Workbooks.Open(File_path+"\\"+names[i])
+        wb = xlapp.Workbooks.Open(FileList)
         wb.Unprotect(pw_str)
         # wb.UnprotectSharing(pw_str)
         xlapp.DisplayAlerts = False
+        wb.RefreshAll()   
+        print(FileList)
         wb.RefreshAll()
-        print(names[i]) 
-        wb.RefreshAll()
+     
         xlapp.CalculateUntilAsyncQueriesDone()
         wb.Save()
         wb.Close()
-    
-xlapp.Quit()
+        #time.sleep(20)
+     xlapp.Quit()
+     return ("All files complete...")
+
+print(Refresh_file(get_path()))
